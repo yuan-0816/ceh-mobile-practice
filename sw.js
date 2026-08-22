d3d5b9b546e013aeed39be30e73404402cdf2b67
yuan-0816 <wj582693@gmail.com>
2026-08-23 00:39:11 +0800

    刪除中英標籤

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ceh-v12';
-const ASSETS=['./','./index.html','./styles.css?v=9','./app.js?v=9','./manifest.webmanifest','./data/questions.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='ceh-v13';
+const ASSETS=['./','./index.html','./styles.css?v=10','./app.js?v=10','./manifest.webmanifest','./data/questions.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
